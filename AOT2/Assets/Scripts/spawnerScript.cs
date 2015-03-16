bb96c1f2789bd1b3d92e9b6438b8b75326bf4a94
sperry94 <sepe4375@colorado.edu>
2015-03-16 11:08:43 -0600

    Triangles Spawning and Moving

---
+++ AOT2/Assets/Scripts/spawnerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnerScript : MonoBehaviour {
+
+	public float spawnt = 1;
+	public GameObject Triangle;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("newTriangle", spawnt, spawnt);
+	}
+
+	void newTriangle()
+	{
+		float left = transform.position.x;
+			float right = transform.position.x+GetComponent<Renderer>().bounds.size.x;
+		/*float top = transform.position.y + GetComponent<Renderer>().bounds.size.y/2;
+		float bottom = transform.position.x - GetComponent<Renderer>().bounds.size.y/2;*/
+		Vector2 spawnp = new Vector2(Random.Range(left,right),transform.position.y);
+		Instantiate (Triangle, spawnp, Quaternion.identity);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ AOT2/Assets/Scripts/triangleScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triangleScript : MonoBehaviour {
+
+	void Start () {
+
+		Vector2 speed = GetComponent<Rigidbody2D>().velocity;
+
+		speed.y = -5;
+
+		GetComponent<Rigidbody2D> ().velocity = speed;
+
+		Destroy (gameObject,3);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
