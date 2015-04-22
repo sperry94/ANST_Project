1b1630f226853429e8132b2f8fb38a141ffce839
sperry94 <sepe4375@colorado.edu>
2015-04-22 16:47:59 -0600

    syncing files not added

---
+++ AOT2/Assets/Scripts/ManagerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManagerScript : MonoBehaviour {
+
+	public GUIText scoreText;
+
+	int score;
+
+	public static int totScoreInt;
+
+	void Start () {
+		score = 0;
+		totScoreInt = 0;
+		scoreText.text = "Score: 0";
+	}
+	
+	public void AddScore(int x)
+	{
+		score += x;
+	}
+	
+	void Update () 
+	{
+		float totScore = score + Time.timeSinceLevelLoad;
+		totScoreInt = (int)totScore;
+		scoreText.text = "Score: " + totScoreInt; 
+	}
+}
